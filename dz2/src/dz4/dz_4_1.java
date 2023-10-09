e2075e77088c25a283bb8873bcbd8136273a69f3
Danila <mama9080908055@gmail.com>
2023-10-09 17:03:04 +0500

    Дз4_1

--- dz2/src/dz4/dz_4_1.java
+++ dz2/src/dz4/dz_4_1.java
@@ -1,10 +1,17 @@
 package dz4;
 
+import java.util.Scanner;
+
 public class dz_4_1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите кол-во циклов");
+        int x = scanner.nextInt();
+        System.out.println("Введите шаг");
+        int k = scanner.nextInt();
         int n = 0;
-        for (int i = 0; i <= 9; i++) {
-            n += 2;
+        for (int i = 1; i <= x; i++) {
+            n += k;
             System.out.println(n);
         }
     }
