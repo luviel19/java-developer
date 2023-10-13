4ceb488e1c2c0c10291e2d9bc6af7e9a6faf4993
Danila <mama9080908055@gmail.com>
2023-10-13 17:36:34 +0500

    Домашнее задание 5

--- dz2/src/dz5/Dz0504.java
+++ dz2/src/dz5/Dz0504.java
@@ -1,8 +1,22 @@
 package dz5;
 
+import static java.lang.Math.max;
+
 public class Dz0504 {
     public static void main(String[] args) {
+        int[] task04 =  {88, 34, 12, 55, 90, 4, 10, 44};
 
+        int max_sum = 0;
+        for (int i = 0; i < 4; ++i) {
+            max_sum += task04[i];
+        }
+        int current_sum = max_sum;
 
+        for (int i = 1; i < task04.length; ++i) {
+            current_sum += task04[(i+3)%task04.length] - task04[i - 1];
+            max_sum = max(max_sum, current_sum);
+        }
+        System.out.println(max_sum);
     }
-}
+    }
+
