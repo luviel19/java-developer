9f1cd480dcf3670c17bef0a0b94e357a4fab636b
Danila <mama9080908055@gmail.com>
2023-10-13 15:13:20 +0500

    Правки Dz0602

--- dz2/src/dz6/Dz0602.java
+++ dz2/src/dz6/Dz0602.java
@@ -2,14 +2,13 @@ package dz6;
 import java.util.Arrays;
 public class Dz0602 {
     public static void main(String[] args) {
-        String str1 = "Java DeVeLOPer";
-        String str2 = "VaJa VeDePoLeR";
-        char[] arrStr1 = str1.toLowerCase().toCharArray();
-        char[] arrStr2 =str2.toLowerCase().toCharArray();
-        Arrays.sort(arrStr1);
-        Arrays.sort(arrStr2);
-        str1 = Arrays.toString(arrStr1);
-        str2 = Arrays.toString(arrStr2);
-        System.out.println(str1.equals(str2));
+        String str1 = "JavA";
+                String str2 = "avaj";
+        boolean areAnagrams = Arrays.equals(
+                str1.toLowerCase().chars().sorted().toArray(),
+                str2.toLowerCase().chars().sorted().toArray()
+        );
+        System.out.println(areAnagrams);;
     }
 }
+
